6f6e29426a585dcd27763b0bafb10214b924ba4c
oalsing <oscar.alsing@gmail.com>
2018-01-08 15:51:54 +0100

    adds submodule for data scraping and updates data fetch in main and utilities

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 ### Run the program ###
 ```python
+git submodule update --init --recursive
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 jupyter notebook
+```
+
+### Update submodules ###
+````sh
+git submodule update --recursive
 ```
--- utilities.py
+++ utilities.py
@@ -17,27 +17,6 @@ def create_data_folder():
         os.makedirs("data")
 
 
-def fetch_historical_bitcoin_data_to_date():
-    stop_date = get_date_from_current(offset=0)
-    url = ("https://api.coindesk.com/v1/bpi/historical/close.json?start=%s&end=%s" % (bitcoin_start_date, stop_date))
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    headers = {'User-Agent': user_agent}
-    request = urllib.request.Request(url, None, headers)
-
-    create_data_folder()
-    currency_file_path = 'data/BTC_%s_%s.csv' % (bitcoin_start_date, stop_date)
-
-    if not os.path.exists(currency_file_path):
-        with urllib.request.urlopen(request) as url:
-            data = json.loads(url.read().decode())
-            with open(currency_file_path, 'w') as price_file:
-                price_file.write("date,close\n")
-                for date in data['bpi']:
-                    price_file.write("%s,%s\n" % (date, data['bpi'][date]))
-
-
 def get_dataset(currency="BTC"):
-    if currency == "BTC":
-        return pd.read_csv('data/BTC_%s_%s.csv' % (bitcoin_start_date,
-                                                   get_date_from_current(offset=0)),
-                           header=0)
+    folder_path = "CryptoAI-CoinMarketCapHistoricalDataScraper/data/%s.csv" % currency
+    return pd.read_csv(folder_path, header=0)
